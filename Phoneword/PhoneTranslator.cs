8903d37dd39ff41bb2e29271b775ff6e76b201e7
dmitrijsgnennijsCTS <32061@corbytechnicalschool.org>
2018-06-06 13:38:55 +0100

    phonetranslator

--- Phoneword/PhoneTranslator.cs
+++ Phoneword/PhoneTranslator.cs
@@ -4,18 +4,25 @@ namespace Core
 {
     public static class PhonewordTranslator
     {
+        // The ToNumber function which takes in letters and translates into numbers
         public static string ToNumber(string raw)
         {
+            // If entry label entry then return nothing
             if (string.IsNullOrWhiteSpace(raw))
                 return null;
 
+            // Uppercase the string 
             raw = raw.ToUpperInvariant();
 
+            // Create a new variable that will store the characters as a string
             var newNumber = new StringBuilder();
+            // Run a for loop for each character (c) in the raw string
             foreach (var c in raw)
             {
+                //If the character matches with any of these then add it to the number variable
                 if (" -0123456789".Contains(c))
                     newNumber.Append(c);
+
                 else
                 {
                     var result = TranslateToNumber(c);
